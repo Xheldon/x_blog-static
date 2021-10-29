b666d6e7fc9fef753a4291d6a37ce4691d7e69df
Xheldon <c1006044256@gmail.com>
2021-10-29 12:38:43 +0800

    add ci

--- upload.js
+++ upload.js
@@ -20,7 +20,7 @@ module.exports = ({github, context}) => {
     const _addAndModifyList = [];
     const _deleteList = [];
     // console.log('github 参数:', github);
-    console.log('contex 参数:', context);
+    console.log('contex 参数:', context.payload.commits);
     // console.log('procss 参数:', process.env);
     // console.log('GITHUB_EVENT 参数:', GITHUB_EVENT);
     return;
